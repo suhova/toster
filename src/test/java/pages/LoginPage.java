f275cfa917515df85b5f9554aa8695c785238fbe
Kucareva <emilija.kucareva@corp.mail.ru>
2019-03-22 14:18:50 +0300

    Initial commit

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage{
+
+    @Override
+    boolean check(WebDriver driver) {
+        throw new UnsupportedOperationException();
+
+    }
+}
---
+++ src/test/java/pages/UserMainPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class UserMainPage extends BasePage {
+    @Override
+    boolean check(WebDriver driver) {
+        throw new UnsupportedOperationException();
+    }
+}
